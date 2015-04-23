d48f733322fea0512a09d3a51da94910dcc88408
M.Itabashi <itabashi@val.co.jp>
2015-04-23 12:08:21 +0900

    alternativesコマンドでちゃんとjava8に切り替えるよう修正

--- java/recipes/java8.rb
+++ java/recipes/java8.rb
@@ -3,8 +3,22 @@ package 'java-1.8.0-openjdk-devel' do
 end
 bash "alternatives java" do
   user "root"
-  
-  EOH
+  code <<-EOC
+    OLDIFS=IFS
+    IFS="
+"
+    for i in `alternatives --display java`; do
+      target=`echo $i | cut -d " " -f 1`
+      if [ -n "${target}" ]; then 
+        if [ `expr ${target} : '/'` -eq 1 ]; then
+          if [ `echo ${target} | grep java-1.8.0-openjdk` ]; then
+            alternatives --set java  ${target}
+          fi
+        fi
+      fi
+    done
+    IFS=$OLDIFS
+  EOC
 end
 
 
